e5bf9bbe83eeaac0dfdbb952c6780604171936eb
lim0n3 <simonestaffa96@gmail.com>
2024-02-12 14:25:12 +0100

    remove /horizon

--- app/page.tsx
+++ app/page.tsx
@@ -17,17 +17,38 @@ export default async function Home({
   const previousFrame = getPreviousFrame(searchParams);
 
   const frameMessage = await getFrameMessage(previousFrame.postBody, {
-    hubHttpUrl: "https://nemes.farcaster.xyz:2281",
+    hubRequestOptions: {
+      headers: { api_key: process.env.NEYNAR_API_KEY! },
+    },
     fetchHubContext: true,
   });
 
   if (frameMessage && !frameMessage?.isValid) {
     throw new Error("Invalid frame payload");
   }
+
+  // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+  // example: load the users credentials & check they have an NFT
+
+  // then, when done, return next frame
   return (
     <div className="p-4">
       Farcaster Frames Frenzy 2024 - Explore on{" "}
       <Link href={REDIRECT_LINK}>Airstack</Link>
+      {process.env.NODE_ENV !== "production" && (
+        <Link href={`/debug?url=${BASE_URL}`} className="underline">
+          Debug
+        </Link>
+      )}
+      <FrameContainer
+        pathname={`${BASE_URL}/api/captcha`}
+        postUrl={`${BASE_URL}/api/captcha`}
+        state={null}
+        previousFrame={previousFrame}
+      >
+        <FrameImage src={COVER_IMAGE_URL} />
+        <FrameButton>Mint your Custom OG NFT!</FrameButton>
+      </FrameContainer>
     </div>
   );
 }
